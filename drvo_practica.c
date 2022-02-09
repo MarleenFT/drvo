70b9abad4e1f8dba4775a7c841bf45cfa95d3915
Marleen <marleen.traas@gmail.com>
2022-02-09 14:09:59 +0100

    31to34

--- drvo_practica.c
+++ drvo_practica.c
@@ -1,21 +1,74 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
 
-MODULE_VERSION("0.1");
+MODULE_VERSION("1");
 MODULE_DESCRIPTION("Test module");
 MODULE_AUTHOR("You");
 MODULE_LICENSE("Dual BSD/GPL");
 
-static int hello_init(void)
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+static int      drvo_init(void);
+static void     drvo_exit(void);
+static ssize_t  drvo_read(struct file*, char*, size_t, loff_t*);
+static ssize_t  drvo_write(struct file*, const char*, size_t, loff_t*);
+static int      drvo_open(struct inode*, struct file*);
+static int      drvo_release(struct inode*, struct file*);
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+struct cdev drvo_cdev;
+dev_t       dev = 0;
+
+struct file_operations drvo_file_ops = {
+    .owner      = THIS_MODULE,
+    .read       = drvo_read,
+    .write      = drvo_write,
+    .open       = drvo_open,
+    .release    = drvo_release
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+static int drvo_init(void)
 {
-    printk(KERN_ALERT "Hld\n");
+    printk(KERN_ALERT "Initialization process\n");
+    
+    cdev_init(&drvo_cdev, &drvo_file_ops);
+    cdev_add(&drvo_cdev, dev, 1);
+
     return 0;
 }
 
-static void hello_exit(void)
+static void drvo_exit(void)
 {
-    printk(KERN_ALERT "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
+    printk(KERN_ALERT "Exiting\n");
+    cdev_del(&drvo_cdev);
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+static ssize_t drvo_read(struct file* pf, char* buf, size_t size, loff_t* lof) {
+    printk(KERN_ALERT "Reading %ld\n", size);
+    return 0;
+}
+
+static ssize_t drvo_write(struct file* pf, const char* buf, size_t size, loff_t* lof) {
+    printk(KERN_ALERT "Writing %ld\n", size);
+    return 0;
+}
+
+static int drvo_open(struct inode* iNode, struct file* fp) {
+    printk(KERN_ALERT "Opening\n");
+    return 0;
+}
+
+static int drvo_release(struct inode* iNode, struct file* fp) {
+    printk(KERN_ALERT "Releasing\n");
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+module_init(drvo_init);
+module_exit(drvo_exit);
