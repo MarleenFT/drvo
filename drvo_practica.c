c1715e6d7c114418f869687f1bb432cb9af1cdc0
Marleen <marleen.traas@gmail.com>
2022-03-21 14:04:12 +0100

    New branch after last branch was corrupted

--- drvo_practica.c
+++ drvo_practica.c
@@ -4,6 +4,9 @@
 #include <linux/cdev.h>
 #include <linux/kdev_t.h>
 #include <linux/device.h>
+#include <asm/uaccess.h>
+#include <linux/uaccess.h>
+#include <linux/errno.h>
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -11,6 +14,10 @@ MODULE_VERSION("1");
 MODULE_DESCRIPTION("Test module");
 MODULE_AUTHOR("Marleen");
 MODULE_LICENSE("Dual BSD/GPL");
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#define DRVO_MAX_BUFFER_SIZE 256
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -33,8 +40,11 @@ static int      drvo_release(struct inode*, struct file*);
 
 static struct cdev*    drvo_cdev = NULL;
 static dev_t           drvo_first_dev = 0;
+static int             drvo_major_number = 0;
 static struct class*   drvo_class = NULL;
 static struct device*  drvo_device = NULL;
+
+static struct file* drvo_file = NULL;
 
 
 struct file_operations drvo_file_ops = {
@@ -47,32 +57,50 @@ struct file_operations drvo_file_ops = {
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
+static int      drvo_open_counter = 0;
+static char     drvo_global_buffer[DRVO_MAX_BUFFER_SIZE] = {0};
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+static char *tty_devnode(struct device *dev, umode_t *mode)
+{
+	if (!mode)
+		return NULL;
+	if (dev->devt == MKDEV(drvo_major_number, 0) ||
+		dev->devt == MKDEV(drvo_major_number, 2))
+	*mode = 0666;
+	return NULL;
+}
+
 static int drvo_init(void)
 {
     printk(KERN_WARNING "Driver initialization\n");
 
-    alloc_chrdev_region(&drvo_first_dev, var_base_minor, var_minor_amount, "Drvo_practica");
+    alloc_chrdev_region(&drvo_first_dev, var_base_minor, var_minor_amount, "drvo_practica");
+    drvo_major_number = MAJOR(drvo_first_dev);
+
     drvo_cdev = cdev_alloc();
     drvo_cdev->ops = &drvo_file_ops;
     cdev_add(drvo_cdev, drvo_first_dev, var_minor_amount);
 
-    printk(KERN_INFO "Registered %d device(s) on major number: %d \n", drvo_cdev->count, MAJOR(drvo_cdev->dev));
+    printk(KERN_INFO "Registered %d device(s) on major number: %d \n", drvo_cdev->count, drvo_major_number);
 
-    drvo_class = class_create(THIS_MODULE, "Drvo_class");
+    drvo_class = class_create(THIS_MODULE, "drvo_class");
     if(IS_ERR(drvo_class)) {
         cdev_del(drvo_cdev);
         unregister_chrdev_region(drvo_first_dev, var_minor_amount);
         printk(KERN_ALERT "Could not create class, exiting!\n");
         return PTR_ERR(drvo_class);
     }
+    drvo_class->devnode = tty_devnode;
 
-    drvo_device = device_create(drvo_class, NULL, drvo_first_dev, NULL, "Drvo_device");
+    drvo_device = device_create(drvo_class, NULL, drvo_first_dev, NULL, "drvo_device");
     if(IS_ERR(drvo_device)) {
         class_destroy(drvo_class);
         cdev_del(drvo_cdev);
         unregister_chrdev_region(drvo_first_dev, var_minor_amount);
         printk(KERN_ALERT "Could not create device, exiting!\n");
-        return PTR_ERR(drvo_class);
+        return PTR_ERR(drvo_device);
     }
 
     printk(KERN_INFO "Driver successfully initiated.\n");
@@ -92,18 +120,60 @@ static void drvo_exit(void)
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
-static ssize_t drvo_read(struct file* pf, char* buf, size_t size, loff_t* lof) {
-    printk(KERN_INFO "Reading %ld bytes of data\n", size);
-    return 0;
-}
+static ssize_t drvo_read(struct file* pf, char __user * buf, size_t size, loff_t* lof) {
+    int result = 0;
 
-static ssize_t drvo_write(struct file* pf, const char* buf, size_t size, loff_t* lof) {
-    printk(KERN_INFO "Writing %ld bytes of data\n", size);
+    printk(KERN_INFO "Reading from drvo initiated\n");
+
+    if (*lof > DRVO_MAX_BUFFER_SIZE) {
+        printk(KERN_ALERT "Read position out of bounds, exiting\n");
+        return 0;
+    }
+
+    if (size + *lof > DRVO_MAX_BUFFER_SIZE) {
+        size = DRVO_MAX_BUFFER_SIZE - *lof;
+    }
+
+    result = copy_to_user(buf, drvo_global_buffer + *lof, size);
+    if (result) {
+		printk(KERN_ALERT "Could not read %d bytes, exiting\n", result);
+		return -EINVAL;
+    }
+
+    *lof += size;
+
+    printk(KERN_INFO "Reading %ld bytes of data\n", size);
     return size;
 }
 
+static ssize_t drvo_write(struct file* pf, const char __user * buf, size_t size, loff_t* lof) {
+    int result = 0;
+    int bytes_written = size;
+
+    printk(KERN_INFO "Writing to drvo initiated\n");
+
+    if ( *lof + bytes_written > DRVO_MAX_BUFFER_SIZE )
+    {
+        bytes_written = DRVO_MAX_BUFFER_SIZE - *lof;
+    }
+
+    result = copy_from_user(drvo_global_buffer + *lof, buf, bytes_written);
+    if (result) {
+		printk(KERN_ALERT "Could not write %d bytes, exiting\n", result);
+		return -EINVAL;
+    }
+
+    *lof += bytes_written;
+
+    printk(KERN_INFO "Written %d bytes of data\n", bytes_written);
+
+    return bytes_written;
+}
+
 static int drvo_open(struct inode* iNode, struct file* fp) {
+    drvo_open_counter++;
     printk(KERN_INFO "Driver opening initiated\n");
+    printk(KERN_INFO "Open counter: %d\n", drvo_open_counter);
     return 0;
 }
 
--- read_write_file.c
+++ read_write_file.c
@@ -4,10 +4,10 @@
 #define FILE_NAME   "/dev/drvo_practica"
 #define MAX_SIZE    255
 
-void write_to_file(const char* p_data) {
+void write_to_file(const char* p_data, int size) {
     FILE* fp = fopen(FILE_NAME, "w");
 
-    fprintf(fp, "%s", p_data);
+    fwrite(p_data, 1, size, fp);
 
     fclose(fp);
 }
@@ -23,8 +23,8 @@ void read_from_file_print() {
 }
 
 int main() {
-    write_to_file("AAAAAAAAAAAAAAAAAAA\n");
-    read_from_file_print();
+    write_to_file("AAAAAAAAAAAAAAAAAAA\n", 21);
+    // read_from_file_print();
 
     return 0;
 }
