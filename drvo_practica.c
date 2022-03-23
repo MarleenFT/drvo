27c881419ddbe26e5188e4d397877ff00c518803
MarleenFT <marleen.traas@gmail.com>
2022-03-23 11:16:10 +0100

    Added ioctl

--- drvo_practica.c
+++ drvo_practica.c
@@ -7,6 +7,9 @@
 #include <asm/uaccess.h>
 #include <linux/uaccess.h>
 #include <linux/errno.h>
+#include <linux/gfp.h>
+#include <linux/slab.h>
+#include <linux/ioctl.h>
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -17,7 +20,7 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
-#define DRVO_MAX_BUFFER_SIZE 256
+#define DRVO_MAX_BUFFER_SIZE 255
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -29,12 +32,14 @@ module_param_named(minor_amount, var_minor_amount, int, 0664);
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
-static int drvo_init(void);
-static void drvo_exit(void);
-static ssize_t drvo_read(struct file *, char *, size_t, loff_t *);
-static ssize_t drvo_write(struct file *, const char *, size_t, loff_t *);
-static int drvo_open(struct inode *, struct file *);
-static int drvo_release(struct inode *, struct file *);
+static int      drvo_init(void);
+static void     drvo_exit(void);
+static ssize_t  drvo_read(struct file *, char *, size_t, loff_t *);
+static ssize_t  drvo_write(struct file *, const char *, size_t, loff_t *);
+static loff_t   drvo_llseek(struct file *, loff_t, int);
+static long     drvo_ioctl(struct file *, unsigned int, unsigned long);
+static int      drvo_open(struct inode *, struct file *);
+static int      drvo_release(struct inode *, struct file *);
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -44,19 +49,19 @@ static        int        drvo_major_number = 0;
 static struct class     *drvo_class = NULL;
 static struct device    *drvo_device = NULL;
 
-static struct file      *drvo_file = NULL;
-
 struct file_operations drvo_file_ops = {
     .owner = THIS_MODULE,
     .read = drvo_read,
     .write = drvo_write,
     .open = drvo_open,
-    .release = drvo_release};
+    .release = drvo_release,
+    .llseek = drvo_llseek,
+    .unlocked_ioctl = drvo_ioctl
+};
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
 static int drvo_open_counter = 0;
-static char drvo_global_buffer[DRVO_MAX_BUFFER_SIZE] = {0};
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -69,11 +74,14 @@ static char *tty_devnode(struct device *dev, umode_t *mode)
         *mode = 0666;
     return NULL;
 }
+
 static int my_dev_uevent(struct device *dev, struct kobj_uevent_env *env)
 {
     add_uevent_var(env, "DEVMODE=%#o", 0666);
     return 0;
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 static int drvo_init(void)
 {
@@ -82,8 +90,6 @@ static int drvo_init(void)
     alloc_chrdev_region(&drvo_first_dev, var_base_minor, var_minor_amount, "drvo_practica");
     drvo_major_number = MAJOR(drvo_first_dev);
 
-    // drvo_cdev = cdev_alloc();
-    // drvo_cdev->ops = &drvo_file_ops;
     cdev_init(&drvo_cdev, &drvo_file_ops); 
     cdev_add(&drvo_cdev, drvo_first_dev, var_minor_amount);
 
@@ -110,8 +116,6 @@ static int drvo_init(void)
         return PTR_ERR(drvo_device);
     }
 
-
-
     printk(KERN_INFO "Driver successfully initiated.\n");
 
     return 0;
@@ -132,31 +136,29 @@ static void drvo_exit(void)
 static ssize_t drvo_read(struct file *pf, char __user *buf, size_t size, loff_t *lof)
 {
     int result = 0;
+    int bytes_read = 0;
 
-    printk(KERN_INFO "Reading from drvo initiated, %ld, %lld\n", size, *lof);
+    printk(KERN_INFO "Reading from drvo initiated\n");
 
-    if (*lof > DRVO_MAX_BUFFER_SIZE)
+    if (*lof > DRVO_MAX_BUFFER_SIZE || *lof < 0)
     {
         printk(KERN_ALERT "Read position out of bounds, exiting\n");
         return 0;
     }
 
-    if (size + *lof > DRVO_MAX_BUFFER_SIZE)
-    {
-        size = DRVO_MAX_BUFFER_SIZE - *lof;
-    }
+    bytes_read = ((size + *lof) > DRVO_MAX_BUFFER_SIZE) ? (DRVO_MAX_BUFFER_SIZE - *lof) : size;
 
-    result = copy_to_user(buf, drvo_global_buffer + *lof, size);
+    result = copy_to_user(buf, pf->private_data + *lof, bytes_read);
     if (result)
     {
         printk(KERN_ALERT "Could not read %d bytes, exiting\n", result);
         return -EINVAL;
     }
 
-    *lof += size;
+    *lof += bytes_read;
 
-    printk(KERN_INFO "Reading %ld bytes of data\n", size);
-    return size;
+    printk(KERN_INFO "Reading %d bytes of data\n", bytes_read);
+    return bytes_read;
 }
 
 static ssize_t drvo_write(struct file *pf, const char __user *buf, size_t size, loff_t *lof)
@@ -164,14 +166,14 @@ static ssize_t drvo_write(struct file *pf, const char __user *buf, size_t size, 
     int result = 0;
     int bytes_written = size;
 
-    printk(KERN_INFO "Writing to drvo initiated, %ld, %lld\n", size, *lof);
+    printk(KERN_INFO "Writing to drvo initiated\n");
 
     if (*lof + bytes_written > DRVO_MAX_BUFFER_SIZE)
     {
         bytes_written = DRVO_MAX_BUFFER_SIZE - *lof;
     }
 
-    result = copy_from_user(drvo_global_buffer + *lof, buf, bytes_written);
+    result = copy_from_user(pf->private_data + *lof, buf, bytes_written);
     if (result)
     {
         printk(KERN_ALERT "Could not write %d bytes, exiting\n", result);
@@ -180,21 +182,56 @@ static ssize_t drvo_write(struct file *pf, const char __user *buf, size_t size, 
 
     *lof += bytes_written;
 
-    printk(KERN_INFO "Written %d, %lld bytes of data\n", bytes_written, *lof);
+    printk(KERN_INFO "Written %d bytes of data\n", bytes_written);
 
     return bytes_written;
+}
+
+static loff_t drvo_llseek(struct file *fp, loff_t off, int whence) {
+    loff_t newpos;
+
+	printk(KERN_INFO "Seeking in data initiated\n");
+
+	switch (whence)
+	{
+	case 0: /* SEEK_SET */
+		newpos = off;
+		break;
+	case 1: /* SEEK_CUR */
+		newpos = fp->f_pos + off;
+		break;
+	case 2: /* SEEK_END */
+		newpos = DRVO_MAX_BUFFER_SIZE - off;
+		break;
+	default: /* can't happen */
+		return -EINVAL;
+	}
+    if( newpos > DRVO_MAX_BUFFER_SIZE ) newpos = DRVO_MAX_BUFFER_SIZE;
+    if( newpos < 0 ) newpos = 0;
+	fp->f_pos = newpos;
+	return newpos;
+}
+
+static long drvo_ioctl(struct file *fp, unsigned int cmd, unsigned long arg) {
+    printk(KERN_INFO "Driver ioctl initiated\n");
+    printk(KERN_INFO "CMD: %d, ARG: %ld\n", cmd, arg);
+    return 0;
 }
 
 static int drvo_open(struct inode *iNode, struct file *fp)
 {
     printk(KERN_INFO "Driver opening initiated\n");
     printk(KERN_INFO "Open counter: %d\n", ++drvo_open_counter);
+
+    fp->private_data = kmalloc(DRVO_MAX_BUFFER_SIZE, GFP_KERNEL);
+
     return 0;
 }
 
 static int drvo_release(struct inode *iNode, struct file *fp)
 {
     printk(KERN_INFO "Driver release initiated\n");
+    kfree(fp->private_data);
     return 0;
 }
 
--- read_write_file.c
+++ read_write_file.c
@@ -4,29 +4,63 @@
 #include <unistd.h> 
 #include <sys/types.h> 
 #include <fcntl.h> 
+#include <sys/ioctl.h>
 
 #define FILE_NAME   "/dev/drvo_device"
 #define MAX_SIZE    255
 
-int readWriteFile = 0;
-char tempRead[255];
+int     readWriteFile = 0;
+int     r2 = 0;
+char    tempRead1[255];
+char    tempRead2[255];
 
-void write_to_file(const char* p_data, int size) {
-    readWriteFile = open(FILE_NAME, O_WRONLY);
-    write(readWriteFile, p_data, size);
-    close(readWriteFile);
+void write_to_file(int f, const char* p_data, int size) {
+    write(f, p_data, size);
 }
 
-void read_from_file_print() {
-    readWriteFile = open(FILE_NAME, O_RDONLY);
-    read(readWriteFile, tempRead, sizeof(tempRead));
-    printf("%s\r\n", tempRead);
-    close(readWriteFile);
+void read_from_file_print1(int f, int size) {
+    read(f, tempRead1, size);
+    printf("%s\n", tempRead1);
+}
+
+void read_from_file_print2(int f, int size) {
+    read(f, tempRead2, size);
+    printf("%s\n", tempRead2);
+}
+
+void seek_data(int f, int place) {
+    lseek(f, place, SEEK_SET);
 }
 
 int main() {
-    write_to_file("a", 2);
-    read_from_file_print();
+    readWriteFile = open(FILE_NAME, O_RDWR);
+    r2 = open(FILE_NAME, O_RDWR);
+
+    ioctl(readWriteFile, 1, 200);
+
+    write_to_file(readWriteFile, "a", 1);
+    write_to_file(r2, "B", 1);
+
+    seek_data(readWriteFile, 0);
+    seek_data(r2, 0);
+
+    read_from_file_print1(readWriteFile, 1);
+    read_from_file_print2(r2, 1);
+
+    seek_data(readWriteFile, 6);
+    seek_data(r2, 3);
+
+    write_to_file(readWriteFile, "c", 1);
+    write_to_file(r2, "D", 1);
+
+    seek_data(readWriteFile, 6);
+    seek_data(r2, 3);
+
+    read_from_file_print1(readWriteFile, 1);
+    read_from_file_print2(r2, 1);
+
+    close(readWriteFile);
+    close(r2);
 
     return 0;
 }
